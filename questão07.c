a2fd446302a655dd122467c1e243becf57d43de7
Anthony-Emanuel <anthony.211106@gmail.com>
2026-09-15 11:06:49 -0400

    Add age classification and validation in questão07.c

--- questão07.c
+++ questão07.c
@@ -1,1 +1,26 @@
+#include <stdio.h>
 
+int main() {
+    int idade;
+
+    printf("Digite a idade da pessoa: ");
+    scanf("%d", &idade);
+
+    if (idade >= 0 && idade <= 12) {
+        printf("Faixa etaria: Crianca\n");
+    } else if (idade <= 17) {
+        printf("Faixa etaria: Adolescente\n");
+    } else if (idade <= 59) {
+        printf("Faixa etaria: Adulto\n");
+    } else if (idade >= 60) {
+        printf("Faixa etaria: Idoso\n");
+    } else {
+        printf("Idade invalida.\n");
+        return 0;
+    }
+
+    printf("Maior de idade: %s\n",
+           idade >= 18 ? "Sim" : "Nao");
+
+    return 0;
+}
